89edb9f7901539bac1f2ffc7da79782e7ecdc413
rocky <rb@dustyfeet.com>
2020-11-29 01:04:29 -0500

    Better tree formatting

--- mathicsscript/format.py
+++ mathicsscript/format.py
@@ -58,32 +58,61 @@ def format_output(obj, expr, format=None):
     return boxes
 
 
-def hierarchy_pos(G, root=None, width=1.0, vert_gap=0.2, vert_loc=0, xcenter=0.5):
+def hierarchy_pos(
+    G, root=None, width=1.0, vert_gap=0.2, vert_loc=0, leaf_vs_root_factor=0.5
+):
 
-    """
-    From Joel's answer at https://stackoverflow.com/a/29597209/2966723.
-    Licensed under Creative Commons Attribution-Share Alike
+    """From EoN (Epidemics on Networks): a fast, flexible Python package
+    for simulation, analytic approximation, and analysis of epidemics
+    on networks
+    https://joss.theoj.org/papers/10.21105/joss.01731
 
     If the graph is a tree this will return the positions to plot this in a
     hierarchical layout.
 
-    G: the graph (must be a tree)
+    Based on Joel's answer at https://stackoverflow.com/a/29597209/2966723,
+    but with some modifications.
 
-    root: the root node of current branch
-    - if the tree is directed and this is not given,
-      the root will be found and used
-    - if the tree is directed and this is given, then
-      the positions will be just for the descendants of this node.
-    - if the tree is undirected and not given,
-      then a random choice will be used.
+    We include this because it may be useful for plotting transmission trees,
+    and there is currently no networkx equivalent (though it may be coming soon).
 
-    width: horizontal space allocated for this branch - avoids overlap with other branches
+    There are two basic approaches we think of to allocate the horizontal
+    location of a node.
 
-    vert_gap: gap between levels of hierarchy
+    - Top down: we allocate horizontal space to a node.  Then its ``k``
+      descendants split up that horizontal space equally.  This tends to result
+      in overlapping nodes when some have many descendants.
+    - Bottom up: we allocate horizontal space to each leaf node.  A node at a
+      higher level gets the entire space allocated to its descendant leaves.
+      Based on this, leaf nodes at higher levels get the same space as leaf
+      nodes very deep in the tree.
 
-    vert_loc: vertical location of root
+    We use use both of these approaches simultaneously with ``leaf_vs_root_factor``
+    determining how much of the horizontal space is based on the bottom up
+    or top down approaches.  ``0`` gives pure bottom up, while 1 gives pure top
+    down.
+
+
+    :Arguments:
+
+    **G** the graph (must be a tree)
+
+    **root** the root node of the tree
+    - if the tree is directed and this is not given, the root will be found and used
+    - if the tree is directed and this is given, then the positions will be
+      just for the descendants of this node.
+    - if the tree is undirected and not given, then a random choice will be used.
+
+    **width** horizontal space allocated for this branch - avoids overlap with other branches
+
+    **vert_gap** gap between levels of hierarchy
+
+    **vert_loc** vertical location of root
+
+    **leaf_vs_root_factor**
 
     xcenter: horizontal location of root
+
     """
     if not nx.is_tree(G):
         raise TypeError("cannot use hierarchy_pos on a graph that is not a tree")
@@ -97,7 +126,17 @@ def hierarchy_pos(G, root=None, width=1.0, vert_gap=0.2, vert_loc=0, xcenter=0.5
             root = random.choice(list(G.nodes))
 
     def _hierarchy_pos(
-        G, root, width=1.0, vert_gap=0.2, vert_loc=0, xcenter=0.5, pos=None, parent=None
+        G,
+        root,
+        leftmost,
+        width,
+        leafdx=0.2,
+        vert_gap=0.2,
+        vert_loc=0,
+        xcenter=0.5,
+        rootpos=None,
+        leafpos=None,
+        parent=None,
     ):
         """
         see hierarchy_pos docstring for most arguments
@@ -107,31 +146,81 @@ def hierarchy_pos(G, root=None, width=1.0, vert_gap=0.2, vert_loc=0, xcenter=0.5
 
         """
 
-        if pos is None:
-            pos = {root: (xcenter, vert_loc)}
+        if rootpos is None:
+            rootpos = {root: (xcenter, vert_loc)}
         else:
-            pos[root] = (xcenter, vert_loc)
+            rootpos[root] = (xcenter, vert_loc)
+        if leafpos is None:
+            leafpos = {}
         children = list(G.neighbors(root))
+        leaf_count = 0
         if not isinstance(G, nx.DiGraph) and parent is not None:
             children.remove(parent)
         if len(children) != 0:
-            dx = width / len(children)
-            nextx = xcenter - width / 2 - dx / 2
+            rootdx = width / len(children)
+            nextx = xcenter - width / 2 - rootdx / 2
             for child in children:
-                nextx += dx
-                pos = _hierarchy_pos(
+                nextx += rootdx
+                rootpos, leafpos, newleaves = _hierarchy_pos(
                     G,
                     child,
-                    width=dx,
+                    leftmost + leaf_count * leafdx,
+                    width=rootdx,
+                    leafdx=leafdx,
                     vert_gap=vert_gap,
                     vert_loc=vert_loc - vert_gap,
                     xcenter=nextx,
-                    pos=pos,
+                    rootpos=rootpos,
+                    leafpos=leafpos,
                     parent=root,
                 )
-        return pos
+                leaf_count += newleaves
 
-    return _hierarchy_pos(G, root, width, vert_gap, vert_loc, xcenter)
+            leftmostchild = min((x for x, y in [leafpos[child] for child in children]))
+            rightmostchild = max((x for x, y in [leafpos[child] for child in children]))
+            leafpos[root] = ((leftmostchild + rightmostchild) / 2, vert_loc)
+        else:
+            leaf_count = 1
+            leafpos[root] = (leftmost, vert_loc)
+        #        pos[root] = (leftmost + (leaf_count-1)*dx/2., vert_loc)
+        #        print(leaf_count)
+        return rootpos, leafpos, leaf_count
+
+    xcenter = width / 2.0
+    if isinstance(G, nx.DiGraph):
+        leafcount = len(
+            [node for node in nx.descendants(G, root) if G.out_degree(node) == 0]
+        )
+    elif isinstance(G, nx.Graph):
+        leafcount = len(
+            [
+                node
+                for node in nx.node_connected_component(G, root)
+                if G.degree(node) == 1 and node != root
+            ]
+        )
+    rootpos, leafpos, leaf_count = _hierarchy_pos(
+        G,
+        root,
+        0,
+        width,
+        leafdx=width * 1.0 / leafcount,
+        vert_gap=vert_gap,
+        vert_loc=vert_loc,
+        xcenter=xcenter,
+    )
+    pos = {}
+    for node in rootpos:
+        pos[node] = (
+            leaf_vs_root_factor * leafpos[node][0]
+            + (1 - leaf_vs_root_factor) * rootpos[node][0],
+            leafpos[node][1],
+        )
+    #    pos = {node:(leaf_vs_root_factor*x1+(1-leaf_vs_root_factor)*x2, y1) for ((x1,y1), (x2,y2)) in (leafpos[node], rootpos[node]) for node in rootpos}
+    xmax = max(x for x, y in pos.values())
+    for node in pos:
+        pos[node] = (pos[node][0] * width / xmax, pos[node][1])
+    return pos
 
 
 def tree_layout(G):
