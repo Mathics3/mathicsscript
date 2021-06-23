f9db6f223d784c724a38c8f5b8d9f9c8b679c190
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2021-06-23 11:08:50 -0300

    Add load_default_settings_files function

--- mathicsscript/__init__.py
+++ mathicsscript/__init__.py
@@ -5,3 +5,15 @@ mathicsscript is a command-line interface to Mathics.
 Copyright 2020-2021 The Mathics Team
 """
 from mathicsscript.version import __version__
+
+
+def load_default_settings_files(definitions):
+    import os.path as osp
+    from mathics.core.definitions import autoload_files
+
+    root_dir = osp.realpath(osp.dirname(__file__))
+
+    autoload_files(definitions, root_dir, "autoload")
+
+
+__all__ = [__version__, load_default_settings_files]
--- test/test_settings.py
+++ test/test_settings.py
@@ -1,37 +1,44 @@
 # -*- coding: utf-8 -*-
 from .helper import session
 
-import os.path as osp
+from mathicsscript import load_default_settings_files
 
-from mathics.core.definitions import autoload_files, Definitions
 
 def test_settings():
-    import os.path as osp
-    from mathics.core.definitions import autoload_files
-    import mathicsscript
+    load_default_settings_files(session.definitions)
 
-    root_dir = osp.realpath(
-        osp.dirname(mathicsscript.__file__),
+    assert (
+        session.evaluate("Settings`$ShowFullFormInput::usage").to_python()
+        != "Settings`$ShowFullFormInput::usage"
     )
-
-    autoload_files(session.definitions, root_dir, "autoload")
-
-    assert session.evaluate("Settings`$ShowFullFormInput::usage").to_python() != "Settings`$ShowFullFormInput::usage"
 
     assert type(session.evaluate("Settings`$ShowFullFormInput").to_python()) is bool
 
-    assert session.evaluate("Settings`$PygmentsStyle::usage").to_python() != "Settings`$PygmentsStyle::usage"
+    assert (
+        session.evaluate("Settings`$PygmentsStyle::usage").to_python()
+        != "Settings`$PygmentsStyle::usage"
+    )
 
-    assert session.evaluate("Settings`$PygmentsShowTokens::usage").to_python() != "Settings`$PygmentsShowTokens::usage"
+    assert (
+        session.evaluate("Settings`$PygmentsShowTokens::usage").to_python()
+        != "Settings`$PygmentsShowTokens::usage"
+    )
 
     assert type(session.evaluate("Settings`$PygmentsShowTokens").to_python()) is bool
 
-    assert session.evaluate("Settings`$UseUnicode::usage").to_python() != "Settings`$UseUnicode::usage"
+    assert (
+        session.evaluate("Settings`$UseUnicode::usage").to_python()
+        != "Settings`$UseUnicode::usage"
+    )
 
     assert type(session.evaluate("Settings`$UseUnicode").to_python()) is bool
 
-    assert session.evaluate("Settings`MathicsScriptVersion::usage").to_python() != "Settings`MathicsScriptVersion::usage"
+    assert (
+        session.evaluate("Settings`MathicsScriptVersion::usage").to_python()
+        != "Settings`MathicsScriptVersion::usage"
+    )
+
 
 def test_is_not_notebook():
-	# the settings already were loaded
+    # the settings already were loaded
     assert session.evaluate("System`$Notebooks").to_python() == False
