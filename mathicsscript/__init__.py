c424acd8f59934efc4520ccf254b0982b0daa32f
Li Xiang <54926635+Li-Xiang-Ideal@users.noreply.github.com>
2023-11-23 11:27:10 +0800

    Update __init__.py

--- mathicsscript/__init__.py
+++ mathicsscript/__init__.py
@@ -4,11 +4,15 @@ mathicsscript is a command-line interface to Mathics.
 
 Copyright 2020-2021 The Mathics Team
 """
+from ctypes.util import find_library
+
+from mathicsscript.fixcairo import fix_cairo
 from mathicsscript.version import __version__
 
 
 def load_default_settings_files(definitions):
     import os.path as osp
+
     from mathics.core.definitions import autoload_files
 
     root_dir = osp.realpath(osp.dirname(__file__))
@@ -17,3 +21,6 @@ def load_default_settings_files(definitions):
 
 
 __all__ = ["__version__", "load_default_settings_files"]
+
+if not find_library("libcairo-2"):
+    fix_cairo()
