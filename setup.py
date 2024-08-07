9ad43f2815324a0da6685220de1562e48acccaf4
rocky <rb@dustyfeet.com>
2024-08-07 04:28:41 -0400

    Go over pyproject.toml
    Need to put command-line name that ends in ".py" in quotes

--- setup.py
+++ setup.py
@@ -29,34 +29,8 @@ setup(
             "mathicsscript/config.asy",
         ]
     },
-    entry_points={
-        "console_scripts": [
-            "mathicsscript = mathicsscript.__main__:main",
-            "fake_psviewer.py = mathicsscript.fake_psviewer:main",
-        ]
-    },
     long_description_content_type="text/x-rst",
     # don't pack Mathics in egg because of media files, etc.
     zip_safe=False,
-    # metadata for upload to PyPI
-    description="A general-purpose computer algebra system.",
-    license="GPL",
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Mathematics",
-        "Topic :: Scientific/Engineering :: Physics",
-        "Topic :: Software Development :: Interpreters",
-    ],
     # TODO: could also include long_description, download_url,
 )
