d33d96f09ae6895b401a77dc25e3ac24985741e4
rocky <rb@dustyfeet.com>
2024-08-09 19:07:41 -0400

    Get ready for release 7.0.0

--- admin-tools/make-dist.sh
+++ admin-tools/make-dist.sh
@@ -3,11 +3,11 @@ PACKAGE=mathicsscript
 
 # FIXME put some of the below in a common routine
 function finish {
-  cd $owd
+  cd $mathicsscript_owd
 }
 
 cd $(dirname ${BASH_SOURCE[0]})
-owd=$(pwd)
+mathicsscript_owd=$(pwd)
 trap finish EXIT
 
 if ! source ./pyenv-versions ; then
@@ -19,13 +19,11 @@ cd ..
 source $PACKAGE/version.py
 echo $__version__
 
-for pyversion in $PYVERSIONS; do
-    if ! pyenv local $pyversion ; then
-		exit $?
-    fi
-    rm -fr build
-    python setup.py bdist_egg
-    python setup.py bdist_wheel
-done
-
+pyversion=3.11
+if ! pyenv local $pyversion ; then
+    exit $?
+fi
+rm -fr build
+python setup.py bdist_wheel
 python ./setup.py sdist
+finish
