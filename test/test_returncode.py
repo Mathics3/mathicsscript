5de901f883de2d0c2e353608b803023cf10ca532
rocky <rb@dustyfeet.com>
2025-07-17 17:53:53 -0400

    No prompt_toolkit in CI testing...
    Windows and MacOS don't work well with prompt_toolkit in CI testing

--- test/test_returncode.py
+++ test/test_returncode.py
@@ -14,7 +14,15 @@ def test_returncode():
     assert subprocess.run(["mathicsscript", "-c", "Quit[0]"]).returncode == 0
 
     gcd_file = osp.join(get_testdir(), "data", "recursive-gcd.m")
-    assert subprocess.run(["mathicsscript", "-f", gcd_file]).returncode == 0
+    # We add --readline None for OSX and Windows which don't interact well
+    # with tty's in a CI environment. TODO: separate out Ubuntu and
+    # use prompt_readline for that?
+    assert (
+        subprocess.run(
+            ["mathicsscript", "--readline", "None", "-f", gcd_file]
+        ).returncode
+        == 0
+    )
 
 
 if __name__ == "__main__":
