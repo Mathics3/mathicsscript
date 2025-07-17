d6284618a05b7a26563f0c196e8402993d643c2d
R. Bernstein <rocky@users.noreply.github.com>
2025-07-17 18:23:27 -0400

    Make CLI options more like wolframscript (#97)
    * Make CLI options more like wolframscript
    
    * No prompt_toolkit in CI testing...
    
    Windows and MacOS don't work well with prompt_toolkit in CI testing
    
    * Allow '-help' as an alias for '--help'

--- test/test_returncode.py
+++ test/test_returncode.py
@@ -9,12 +9,20 @@ def get_testdir():
 
 
 def test_returncode():
-    assert subprocess.run(["mathicsscript", "-e", "Quit[5]"]).returncode == 5
-    assert subprocess.run(["mathicsscript", "-e", "1 + 2'"]).returncode == 0
-    assert subprocess.run(["mathicsscript", "-e", "Quit[0]"]).returncode == 0
+    assert subprocess.run(["mathicsscript", "-c", "Quit[5]"]).returncode == 5
+    assert subprocess.run(["mathicsscript", "-c", "1 + 2'"]).returncode == 0
+    assert subprocess.run(["mathicsscript", "-c", "Quit[0]"]).returncode == 0
 
     gcd_file = osp.join(get_testdir(), "data", "recursive-gcd.m")
-    assert subprocess.run(["mathicsscript", "-f", gcd_file]).returncode == 0
+    # We add --readline None for OSX and Windows which don't interact well
+    # with tty's in a CI environment. TODO: separate out Ubuntu and
+    # use prompt_readline for that?
+    assert (
+        subprocess.run(
+            ["mathicsscript", "--readline", "None", "-f", gcd_file]
+        ).returncode
+        == 0
+    )
 
 
 if __name__ == "__main__":
